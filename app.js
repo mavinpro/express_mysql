74db37be997db1c5974f2ead9736ab5669f34b1c
mavinpro <marchelinokevin12@outlook.com>
2024-05-03 08:56:10 +0700

    adding session

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 //Tambahkan panggil routes kelas
 var kelasRouter = require("./routes/kelas");
+var sessionRouter = require("./routes/session");
 
 var app = express();
 
@@ -16,9 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "iniadalahkeyrahasiamu",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 },
+  })
+);
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 //Tambahkan kelasRouter
 app.use("/kelas", kelasRouter);
+app.use("/session", sessionRouter);
 
 module.exports = app;
